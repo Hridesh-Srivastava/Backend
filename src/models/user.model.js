65b4569f49652bc491117d7f450ce142eaddbe85
Hridesh Srivastava <hrideshsrivastavaa@outlook.com>
2024-09-28 04:34:20 +0530

    initial commit

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,4 @@
-   import mongoose , {Schema} from "mongoose";
+   import mongoose , { Schema } from "mongoose";
    import jwt from "jsonwebtoken";
    import bcrypt from "bcrypt";
 
@@ -75,3 +75,16 @@ userSchema.methods.generateAccessToken = function (){
     }
  )
 }
+
+userSchema.methods.generateRefreshToken = function (){
+    return jwt.sign({
+        _id : this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+};
+
+export const User = mongoose.model("User" , userSchema);
