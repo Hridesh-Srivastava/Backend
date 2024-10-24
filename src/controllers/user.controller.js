06f9da9a46a83dc480bb98a16770299eac649095
Hridesh Srivastava <hrideshsrivastavaa@outlook.com>
2024-10-24 11:42:10 +0530

    write some controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,25 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
-import { upload } from "../middlewares/multer.middleware.js";
+import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 
 const registerUser = asyncHandler(async (req ,res) => {
-   const {fullname , username , email , password} = req.body;
+   // res.sendStatus(200).json({
+   //    message : "OK",
+   // });
+
+   const {fullname, username, email, password} = req.body;
+//arr.some() takes upto 3 params and return true/false based on any condition setted for it.
+   if([fullname, username, email, password].some((filter) => {
+      return filter?.trim() === ""; //optional chain karke trim kardia
+   })){
+      throw new ApiError(400, "All fields are required!");
+   }
+
+   const existedUser = await User.findOne({ //if user exists 
+      $or: [{email},{password}] //$or check karega first document related to user's email and pass. and simply return kardega
+   });
 });
 
 export { registerUser }
