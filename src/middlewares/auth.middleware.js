3a2b08127a9f48969171766ff9326e64b4dac390
Hridesh Srivastava <hrideshsrivastavaa@outlook.com>
2024-11-05 19:06:02 +0530

    create routes for login and logout

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
 //to check from the server whether the user is verified/authorized user or not.
-export const verfiyJWT = asyncHandler(async(req , res , next) => {
+export const verifyJWT = asyncHandler(async(req , res , next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
 
